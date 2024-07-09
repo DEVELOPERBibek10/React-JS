096e12ace340f3ea4053cbe8ce8af81e981f76a6
DEVELOPERBibek10 <bibekpyakurel7@gmail.com>
2024-07-09 22:12:29 +0545

    Change

--- vite-project/src/components/Navbar.tsx
+++ vite-project/src/components/Navbar.tsx
@@ -84,7 +84,7 @@ const Navbar = ({ user }: { user: boolean }) => {
               {user ? (
                 <li>
                   <button
-                    className="middle none center rounded-xl bg-[#1C64F2] py-2.5  px-6 font-sans text-xs font-bold uppercase text-white shadow-md shadow-[#1C64F2]/20 transition-all hover:shadow-lg hover:shadow-[#1C64F2]/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                    className="middle none center rounded-2xl bg-[#1C64F2] py-2.5  px-6 font-sans text-xs font-bold uppercase text-white shadow-md shadow-[#1C64F2]/20 transition-all hover:shadow-lg hover:shadow-[#1C64F2]/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
                     data-ripple-light="true"
                     onClick={logout}
                   >
